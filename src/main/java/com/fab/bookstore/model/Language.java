b1234357818d8dd0f0ce560de6d326ed7023fd84
fabuela <cfabuela@gmail.com>
2018-08-05 09:32:20 +0200

    model definition

---
+++ src/main/java/com/fab/bookstore/model/Language.java
@@ -0,0 +1,13 @@
+package com.fab.bookstore.model;
+
+public enum Language {
+    ENGLISH,
+    FRENCH,
+    SPANISH,
+    PORTUGUESE,
+    ITALIAN,
+    FINISH,
+    GERMAN,
+    DEUTSCH,
+    RUSSIAN
+}
